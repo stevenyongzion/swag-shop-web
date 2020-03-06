e6e0af312f70508bf3fb81bbe6bdc7abbc0ba802
Steven Yong Koon Loong <STEVENYONG@maxis.com.my>
2020-03-06 09:25:49 +0800

    Updates

---
+++ precache-manifest.69f361a618359d2342e18454f5e140db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd69094fc4246438651310c524a8fd9f",
+    "url": "/swag-shop-web/index.html"
+  },
+  {
+    "revision": "e89811dc2e012dfdda2d",
+    "url": "/swag-shop-web/static/css/main.bc3c3983.chunk.css"
+  },
+  {
+    "revision": "e7ba1692572d3decb21b",
+    "url": "/swag-shop-web/static/js/2.1c621914.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/swag-shop-web/static/js/2.1c621914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89811dc2e012dfdda2d",
+    "url": "/swag-shop-web/static/js/main.85abc07a.chunk.js"
+  },
+  {
+    "revision": "b4d8473d86d9a8394338",
+    "url": "/swag-shop-web/static/js/runtime-main.ae298cb9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/swag-shop-web/static/media/logo.ee7cd8ed.svg"
+  }
+]);
